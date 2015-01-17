3b2dc1831244766202868e1fbb71b87eabe6fd52
Alexi Rahman <alexi.rahman@r76.se>
2015-01-17 17:55:39 +0100

    Changed request requirement to requests>=2.5.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name="pinterest-pinata",
       version="1.1.3",
       description="Pinterest client",
-      install_requires=["requests==2.5.0"],
+      install_requires=["requests>=2.5.0"],
       author="Alexi Rahman",
       author_email="alexi.rahman@r76.se",
       url="http://github.com/odynvolk/pinterest-pinata",
